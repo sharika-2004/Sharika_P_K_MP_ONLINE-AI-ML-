c7fe45c11918778ac1ee5ea924bc1e0c4ee6f6e0
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 12:17:56 +0530

    Create Task 3: Feature engineering.py

---
+++ Adult_Census_Income/Task 3: Feature engineering.py
@@ -0,0 +1,17 @@
+label_encoders = {}
+
+for col in df.columns:
+    if df[col].dtype == 'object':
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        label_encoders[col] = le
+
+df.head()
+
+##OUTPUT
+age	workclass	fnlwgt	education	education-num	marital-status	occupation	relationship	race	sex	capital-gain	capital-loss	hours-per-week	native-country	income
+39	   5	    77516	     9	       13	             4	             0	          1	       4	   1	    2174	          0	           40	             38	           0
+50	   4    	83311	     9	       13              	2	             3	          0	       4	   1	      0	            0	            13	           38	           0
+38	   2	    215646	    11	     9	             0	             5	          1	       4	   1	      0	            0	            40	           38	           0 
+53	   2	    234721	     1	      7	             2	             5	          0	       2	   1	      0	            0	            40	           38	           0
+28	   2	     338409	      9	     13	             2	             9	          5	       2    	0	      0	            0	            40             	4	           0
