94c949e90ef3909b86dd9a53f01d2405acab4449
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 12:19:10 +0530

    Update Task 3: Feature engineering.py

--- Adult_Census_Income/Task 3: Feature engineering.py
+++ Adult_Census_Income/Task 3: Feature engineering.py
@@ -15,3 +15,21 @@ age	workclass	fnlwgt	education	education-num	marital-status	occupation	relations
 38	   2	    215646	    11	     9	             0	             5	          1	       4	   1	      0	            0	            40	           38	           0 
 53	   2	    234721	     1	      7	             2	             5	          0	       2	   1	      0	            0	            40	           38	           0
 28	   2	     338409	      9	     13	             2	             9	          5	       2    	0	      0	            0	            40             	4	           0
+
+
+##CODE
+X = df.drop('income', axis=1)
+y = df['income']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
+
+print(X_train.shape)
+print(X_test.shape)
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
