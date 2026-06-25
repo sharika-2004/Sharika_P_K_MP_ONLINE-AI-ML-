eb47909283bc8655b82e8e03437d8cc87c32dd18
Sharika P K <sharikavipanchika@gmail.com>
2026-06-25 12:19:47 +0530

    Create Task 4: Model Building.py

---
+++ Adult_Census_Income/Task 4: Model Building.py
@@ -0,0 +1,32 @@
+models = {
+    "Logistic Regression": LogisticRegression(max_iter=1000),
+    "Decision Tree": DecisionTreeClassifier(random_state=42),
+    "Random Forest": RandomForestClassifier(random_state=42),
+    "KNN": KNeighborsClassifier(),
+    "SVM": SVC(probability=True)
+}
+
+results = []
+
+for name, model in models.items():
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    y_prob = model.predict_proba(X_test)[:,1]
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_prob)
+
+    results.append([
+        name,
+        accuracy,
+        precision,
+        recall,
+        f1,
+        roc_auc
+    ])
